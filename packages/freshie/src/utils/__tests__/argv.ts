d6cf778c72b84fbf0b7830a5455dbc881c4aed63
Luke Edwards <luke.edwards05@gmail.com>
2020-10-08 13:08:47 -0700

    chore(utils): add `argv` tests

--- packages/freshie/src/utils/__tests__/argv.ts
+++ packages/freshie/src/utils/__tests__/argv.ts
@@ -1,4 +1,5 @@
 import { suite } from 'uvu';
+import { resolve } from 'path';
 import * as assert from 'uvu/assert';
 import * as utils from '../argv';
 
@@ -26,3 +27,61 @@ toBool('should return `true` otherwise`', () => {
 });
 
 toBool.run();
+
+// ---
+
+const normalize = suite('normalize');
+
+normalize('should be a function', () => {
+	assert.type(utils.normalize, 'function');
+});
+
+normalize('should ensure `Argv.Options` has values', () => {
+	const input: Partial<Argv.Options> = {};
+	const output = utils.normalize(null, input);
+	assert.is(output, undefined, 'returns nothing');
+
+	const cwd = resolve('.');
+
+	assert.is(input.cwd, cwd);
+
+	assert.is(input.srcDir, 'src');
+	assert.is(input.destDir, 'build');
+
+	assert.is(input.src, cwd); // "{cwd}/src" missing
+	assert.is(input.dest, resolve(cwd, 'build'));
+
+	assert.is(input.ssr, true);
+	assert.is(input.minify, true);
+});
+
+normalize('should accept `Argv.Options` partial values', () => {
+	const input: Partial<Argv.Options> = {
+		cwd: __dirname,
+		dest: 'output',
+		minify: false,
+	};
+
+	utils.normalize('hello', input);
+
+	const cwd = resolve('.');
+
+	assert.is(input.cwd, __dirname);
+
+	assert.is(input.srcDir, 'hello');
+	assert.is(input.destDir, 'output');
+
+	assert.is(input.src, __dirname); // "{cwd}/src" missing
+	assert.is(input.dest, resolve(cwd, 'output'));
+
+	assert.is(input.ssr, true);
+	assert.is(input.minify, false);
+});
+
+normalize('should accept extra values', () => {
+	const input: Partial<Argv.Options> = {};
+	utils.normalize('', input, { isProd: true });
+	assert.is(input.isProd, true);
+});
+
+normalize.run();
