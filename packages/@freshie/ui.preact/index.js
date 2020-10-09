a96701e326529633e60bc9b8b1087d5e7b48f86a
Luke Edwards <luke.edwards05@gmail.com>
2020-10-09 14:56:21 -0700

    feat(ui.preact): add missing `layout` export;
    - converts [Layout1, Layout1, ..., Route] => View

--- packages/@freshie/ui.preact/index.js
+++ packages/@freshie/ui.preact/index.js
@@ -9,12 +9,17 @@ export function hydrate(Tag, props, target) {
 	root = preact.hydrate(preact.h(Tag, props), target, root);
 }
 
+export function layout(Tags, props={}) {
+	let len=Tags.length, vnode;
+	while (len-- > 0) vnode = preact.h(Tags[len], props, vnode);
+	return vnode;
+}
+
 // ---
 
 import toHTML from 'preact-render-to-string';
 
-export function ssr(Tag, props={}) {
-	let vnode = preact.h(Tag, props);
+export function ssr(vnode) {
 	let body = toHTML(vnode) || '';
 	return { head: '', body };
 }
