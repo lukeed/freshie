0ff1a0df973662db922fdaf3d6de37c9e22a3161
Luke Edwards <luke.edwards05@gmail.com>
2020-10-09 20:38:49 -0700

    fix(ui.svelte): better layout-nesting for ssr mode

--- packages/@freshie/ui.svelte/index.js
+++ packages/@freshie/ui.svelte/index.js
@@ -11,17 +11,23 @@ export function hydrate(Tag, props, target) {
 
 // ---
 
+// ---
+
+import { create_ssr_component } from 'svelte/internal';
+
 export function ssr(Tags, props={}) {
-	let head='', body='';
-	let slots, len=Tags.length;
+	let head='', body='', len=Tags.length;
 
 	if (len > 0) {
+		let ssr = create_ssr_component(($$out, $$props, $$bindings) => {
+			let slots={};
 		while (len-- > 1) {
+				let tmp = Tags[len];
 			// NOTE: you can ONLY use <slot/> in layouts
-			slots = { default: () => Tags[len].render(props, slots) };
+				slots = { default: ($$p) => tmp.$$render($$out, $$p, $$bindings, slots) };
 		}
-
-		let ssr = Tags[0].render(props, slots);
+			return Tags[0].$$render($$out, {}, {}, slots);
+		}).render(props);
 
 		body += ssr.html;
 		head += ssr.head || '';
