58ee07ea9aa95bd15c6429418f82f5fa365b57b6
Luke Edwards <luke.edwards05@gmail.com>
2020-10-10 13:34:19 -0700

    fix(ui.svelte): write css in DOM output

--- packages/@freshie/ui.svelte/config.js
+++ packages/@freshie/ui.svelte/config.js
@@ -1,12 +1,12 @@
 exports.svelte = {
 	hydratable: true,
-	css: false,
 }
 
 // TODO: load CWD/svelte.config.js values
 exports.rollup = function (config, options, context) {
 	config.plugins.push(
 		require('rollup-plugin-svelte')({
+			css: context.ssr ? false : (css => css.write('index.css')),
 			generate: context.ssr ? 'ssr' : 'dom',
 			...options.svelte,
 		})
