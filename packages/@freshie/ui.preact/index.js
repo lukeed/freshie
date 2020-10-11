cad6b4419606c3a5b8b9e896081f82b45caee31f
Luke Edwards <luke.edwards05@gmail.com>
2020-10-11 13:12:18 -0700

    fix(ui.preact): do `hydrate` -> `render` hand-off

--- packages/@freshie/ui.preact/index.js
+++ packages/@freshie/ui.preact/index.js
@@ -1,12 +1,11 @@
 import * as preact from 'preact';
 
-var root;
-export function render(Tag, props, target) {
-	root = preact.render(preact.h(Tag, props), target, root);
+export function render(Tags, props, target) {
+	preact.render(layout(Tags, props), target);
 }
 
-export function hydrate(Tag, props, target) {
-	root = preact.hydrate(preact.h(Tag, props), target, root);
+export function hydrate(Tags, props, target) {
+	preact.hydrate(layout(Tags, props), target);
 }
 
 export function layout(Tags, props={}) {
