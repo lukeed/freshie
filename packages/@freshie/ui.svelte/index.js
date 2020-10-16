926b7dc114f16e54cd096c957c4be71b48bc60bb
Luke Edwards <luke.edwards05@gmail.com>
2020-10-16 07:30:02 -0700

    fix(ui.svelte): build nested layout view

--- packages/@freshie/ui.svelte/index.js
+++ packages/@freshie/ui.svelte/index.js
@@ -1,15 +1,69 @@
 var App;
 
-export function render(Tag, props, target) {
+export function render(Tags, props, target) {
 	if (App) App.$destroy();
-	App = new Tag({ props, target });
+	var Tag = layout(Tags, props);
+	App = Tag({ props, target });
 }
 
-export function hydrate(Tag, props, target) {
-	App = new Tag({ props, target, hydrate: true });
+export function hydrate(Tags, props, target) {
+	var Tag = layout(Tags, props);
+	App = Tag({ props, target, hydrate: true });
 }
 
 // ---
+
+// ---
+
+import { detach, insert, noop, SvelteComponent } from 'svelte/internal';
+
+function slotty(elem) {
+	return function () {
+		var t, frag={};
+		if (elem instanceof SvelteComponent) t = elem;
+		else if (typeof elem === 'function') t = new elem({ $$inline: true });
+		frag = t && t.$$ && t.$$.fragment || frag;
+
+		frag.c = frag.c || noop;
+		frag.l = frag.l || noop;
+
+		frag.m = frag.m || function (target, anchor) {
+			insert(target, elem, anchor);
+		}
+
+		frag.d = frag.d || function (detaching) {
+			if (detaching) detach(t || elem);
+		};
+
+		return frag;
+	};
+}
+
+function bury(props, prev) {
+	return {
+		...props,
+		$$scope: {},
+		$$slots: {
+			default: [ slotty(prev) ]
+		}
+	};
+}
+
+export function layout(Tags, props={}) {
+	var len=Tags.length, last, Tag=Tags[0];
+	while (len-- > 1) {
+		last = new Tags[len]({
+			props: last ? bury(props, last) : props,
+			$$inline: true,
+		});
+	}
+	return function (opts={}) {
+		if (last && opts.props) {
+			opts.props = bury(props, last);
+		}
+		return new Tag(opts);
+	}
+}
 
 // ---
 
@@ -21,11 +75,11 @@ export function ssr(Tags, props={}) {
 	if (len > 0) {
 		let ssr = create_ssr_component(($$out, $$props, $$bindings) => {
 			let slots={};
-		while (len-- > 1) {
+			while (len-- > 1) {
 				let tmp = Tags[len];
-			// NOTE: you can ONLY use <slot/> in layouts
+				// NOTE: you can ONLY use <slot/> in layouts
 				slots = { default: () => tmp.$$render($$out, $$props, $$bindings, slots) };
-		}
+			}
 			return Tags[0].$$render($$out, $$props, $$bindings, slots);
 		}).render(props);
 
