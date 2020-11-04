6e841630650d933d0abdc6bd1682d83298a81f67
Luke Edwards <luke.edwards05@gmail.com>
2020-11-04 15:16:57 -0800

    fix(core): prevent double `preload` for module scripts;
    - TODO: investigate; should be able to have both `preload` variants w/o duplicate request

--- packages/freshie/src/config/plugins/html.ts
+++ packages/freshie/src/config/plugins/html.ts
@@ -60,13 +60,13 @@ export function HTML(template: string, opts: Options = {}): Rollup.Plugin {
 						if (preload) append(dochead, toPreload(filename, 'style'));
 						append(dochead, `<link rel="stylesheet" href="${filename}"/>`);
 					} else if (/\.m?js$/.test(filename)) {
-						if (preload) append(dochead, toPreload(filename, 'script'));
 						if (/esm?/.test(format)) {
 							// TODO(future): "preload" => "modulepreload" *only* when better supported
 							if (preload) append(dochead, `<link rel="modulepreload" href="${filename}"/>`);
 							append(docbody, `<script type="module" src="${filename}"></script>`);
 							append(docbody, `<script nomodule defer src="https://unpkg.com/dimport/nomodule" data-main="${filename}"></script>`);
 						} else {
+							if (preload) append(dochead, toPreload(filename, 'script'));
 							append(docbody, `<script src="${filename}"></script>`);
 						}
 					}
