b673406040356bfa59d1ff5fdc8dcad084a21cce
Luke Edwards <luke.edwards05@gmail.com>
2020-11-04 14:45:35 -0800

    fix(ui.svelte): disable `css-modules` w/ svelte

--- packages/@freshie/ui.svelte/config.js
+++ packages/@freshie/ui.svelte/config.js
@@ -8,17 +8,33 @@ exports.templates = function (config) {
 	config.test = /\.svelte$/;
 }
 
+exports.postcss = function (config) {
+	config.modules = false;
+}
+
 exports.svelte = {
+	emitCss: true,
 	hydratable: true,
+	// TODO: 7.0
+	// compilerOptions: {
+	// 	hydratable: true,
+	// }
 }
 
 // TODO: load CWD/svelte.config.js values
 exports.rollup = function (config, context, options) {
+	/*
+	TODO: 7.0
+	const { compilerOptions } = options.svelte;
+
+	compilerOptions.hydratable = true;
+	compilerOptions.generate = context.ssr ? 'ssr' : 'dom';
+
+	options.svelte.compilerOptions = compilerOptions;
+	options.svelte.emitCss = true;
+	*/
+
 	config.plugins.push(
-		require('rollup-plugin-svelte')({
-			css: context.ssr ? false : (css => css.write('index.css')),
-			generate: context.ssr ? 'ssr' : 'dom',
-			...options.svelte,
-		})
+		require('rollup-plugin-svelte')(options.svelte)
 	);
 }
